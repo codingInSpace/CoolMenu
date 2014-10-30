a67470ef820c10e91a2dcb1aa3bd1e5105f2a715
Jonathan <jonathan.grangien@gmail.com>
2014-10-30 02:03:10 +0100

    added Menu class and splash activity
    Main class should link to splash because Menu links to main?

--- src/com/example/coolmenu/Menu.java
+++ src/com/example/coolmenu/Menu.java
@@ -3,11 +3,19 @@ package com.example.coolmenu;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class Main extends Activity{
-	
+public class Menu extends Activity{
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+	
 }

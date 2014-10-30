a8e9624797f619456b1a97eda452ede8845b04dd
Jonathan <jonathan.grangien@gmail.com>
2014-10-30 02:31:57 +0100

    Added thread for splash activity
    Displays a logo for 5 sec (sleep(5000)), splash.xml. Refers later to main.xml

--- src/com/example/coolmenu/Main.java
+++ src/com/example/coolmenu/Main.java
@@ -1,6 +1,7 @@
 package com.example.coolmenu;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Main extends Activity{
@@ -8,6 +9,23 @@ public class Main extends Activity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		setContentView(R.layout.splash);
+		
+		Thread logoTimer = new Thread(){
+			public void run(){
+				try{
+					sleep(5000);
+					Intent menuIntent = new Intent("com.example.coolmenu.MENU");
+					startActivity(menuIntent);
+				}
+				 catch(InterruptedException e){
+					 e.printStackTrace();
+				 }
+				finally{
+					finish();
+				}
+			}
+		};
+		logoTimer.start(); 
 	}
 }

726d126c9fc0f7bb60565820eafd4f27c8515d97
Jonathan <jonathan.grangien@gmail.com>
2014-11-01 17:10:50 +0100

    Added logomusic
    logoMusic that runs through MediaPlayer class on splash activity. Also, audio files need to have lower case names with underscores only

--- src/com/example/coolmenu/main.java
+++ src/com/example/coolmenu/main.java
@@ -2,15 +2,19 @@ package com.example.coolmenu;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class main extends Activity{
+	MediaPlayer logoMusic;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.splash);
 		
+		logoMusic = MediaPlayer.create(main.this, R.raw.me3startsound);
+		logoMusic.start();
 		Thread logoTimer = new Thread(){
 			public void run(){
 				try{
@@ -28,4 +32,12 @@ public class main extends Activity{
 		};
 		logoTimer.start(); 
 	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		logoMusic.release();
+	}
+	
 }

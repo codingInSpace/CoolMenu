fe85a380c5fedcf4f099b5e9fe1165c2784ff815
Jonathan <jonathan.grangien@gmail.com>
2014-11-01 15:38:12 +0100

    Renamed class names to lower letter correspondent
    Lower letter names supposedly work better for the build tools to generate R-file. project.properties also fiddled around with when getting said generation to work on linux

---
+++ src/com/example/coolmenu/main.java
@@ -0,0 +1,31 @@
+package com.example.coolmenu;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class main extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		
+		Thread logoTimer = new Thread(){
+			public void run(){
+				try{
+					sleep(5000);
+					Intent menuIntent = new Intent("com.example.coolmenu.MENU");
+					startActivity(menuIntent);
+				}
+				 catch(InterruptedException e){
+					 e.printStackTrace();
+				 }
+				finally{
+					finish();
+				}
+			}
+		};
+		logoTimer.start(); 
+	}
+}
